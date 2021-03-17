490c0e261bca3c46987ec99b7f3d2bfc6e0f6b00
Abhijit Apte <abapte@adobe.com>
2021-03-17 20:45:52 +0530

    Fix minor issues

--- MemeMev2.0/MemeDetailViewController.swift
+++ MemeMev2.0/MemeDetailViewController.swift
@@ -11,7 +11,6 @@ class MemeDetailViewController: UIViewController {
 	
 	var meme: Meme?
 	
-	@IBOutlet weak var originalImage: UIImageView!
 	@IBOutlet weak var topLabel: UILabel!
 	@IBOutlet weak var bottomLabel: UILabel!
 	@IBOutlet weak var memedImage: UIImageView!
@@ -24,20 +23,8 @@ class MemeDetailViewController: UIViewController {
 	
 	override func viewWillAppear(_ animated: Bool) {
 		self.memedImage?.image = meme?.memedImage
-		self.originalImage?.image = meme?.originalImage
 		self.topLabel?.text = meme?.topText
 		self.bottomLabel?.text = meme?.bottomText
 	}
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- MemeMev2.0/SentMemesTableViewController.swift
+++ MemeMev2.0/SentMemesTableViewController.swift
@@ -19,11 +19,6 @@ class SentMemesTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 		self.navigationItem.title = "Sent Memes"
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 	
 	override func viewWillAppear(_ animated: Bool) {
@@ -47,13 +42,18 @@ class SentMemesTableViewController: UITableViewController {
 		
 		return cell
     }
-    
+	
+	// MARK: - Collection view delegate
+	
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		self.detailRowIndex = (indexPath as NSIndexPath).row
 		performSegue(withIdentifier: "ShowSentMemeDetailFromTableViewCell", sender: self)
 		
 	}
-	
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 		if segue.identifier == "ShowSentMemeDetailFromTableViewCell" {
 			let detailController = segue.destination as! MemeDetailViewController
@@ -61,50 +61,5 @@ class SentMemesTableViewController: UITableViewController {
 			self.detailRowIndex = nil
 		}
 	}
-	
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
